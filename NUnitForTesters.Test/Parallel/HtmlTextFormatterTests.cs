04cae4b1cbf28a7d81ed1a6a5a042f73bf2a8a15
Michal Dobrzycki <dobrzycki@gmail.com>
2022-08-25 11:14:46 +0200

    Reverted solutions

--- NUnitForTesters.Test/Parallel/HtmlTextFormatterTests.cs
+++ NUnitForTesters.Test/Parallel/HtmlTextFormatterTests.cs
@@ -7,5 +7,65 @@ using System.Threading;
 
 namespace NUnitForTesters.Test.Parallel
 {
+    // run all tests in parallel
+    [Parallelizable(ParallelScope.Children)]
+    public class HtmlTextFormatterTests
+    {
+        HtmlTextFormatter htf;
+        [SetUp]
+        public void SetUp()
+        {
+            htf = new HtmlTextFormatter();
+        }
 
+        [Test]
+        public void TestBoldStuff()
+        {
+            var result = htf.FormatAsBold("Okazja!!!");
+            StringAssert.IsMatch(result, "<b>Okazja!!!</b>");
+        }
+
+        [Test]
+        public void TestItalicStuff()
+        {
+            var result = htf.FormatAsItalic("Okazja!!!");
+            StringAssert.IsMatch(result, "<i>Okazja!!!</i>");
+        }
+
+        [Test]
+        public void TestSmallStuff()
+        {
+            var result = htf.FormatAsSmall("Okazja!!!");
+            StringAssert.IsMatch(result, "<small>Okazja!!!</small>");
+        }
+
+        [Test]
+        public void TestDeletedStuff()
+        {
+            var result = htf.FormatAsDeleted("Okazja!!!");
+            StringAssert.IsMatch(result, "<del>Okazja!!!</del>");
+        }
+
+
+        [Test]
+        public void TestSubscriptStuff()
+        {
+            var result = htf.FormatAsSubscript("Okazja!!!");
+            StringAssert.IsMatch(result, "<sub>Okazja!!!</sub>");
+        }
+
+
+        [Test]
+        public void TestCustomStuff()
+        {
+            String result = htf.FormatAsCustomTag("Okazja!!!", "div");
+            StringAssert.IsMatch(result, "<div>Okazja!!!</div>");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Thread.Sleep(1500);
+        }
+    }
 }
